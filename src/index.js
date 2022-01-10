706a199ff74711a0bb1573e91f98ce37f06677b7
Julian Swerdlin <julianswer36@gmail.com>
2022-01-10 23:14:50 +0100

    error module not found in index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import paymentRoutes from "./routes/payment.routes";
 
 const app = express();
+
+app.use(paymentRoutes);
 
 app.listen(3000);
 console.log('Server on port', 3000);
