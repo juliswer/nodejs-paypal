4341ab6c824c4f1fb260545f0f65fb399fc3dd78
Julian Swerdlin <julianswer36@gmail.com>
2022-01-11 23:06:48 +0100

    new

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,5 +1,33 @@
+import axios from 'axios'
+import {PAYPAL_API, PAYPAL_API_CLIENT, PAYPAL_API_SECRET} from '../config.js';
+
 export const createOrder = (req, res) => {
-    res.send('creating order')
+    const order = {
+        intent: 'CAPTURE',
+        purchase_units: [
+            {
+                amount: {
+                    currency_code: 'USD',
+                    value: '105.70'
+                },
+                description: "teclado de computadora",
+            }
+        ],
+        application_context: {
+            brand_name: "mycompany.com",
+            landing_page: "LOGIN",
+            user_action: "PAY_NOW",
+            return_url: 'http://localhost:4000/capture-order',
+            cancel_url: 'http://localhost:4000/cancel-order'
+        }
+    }
+
+    axios.post(`${PAYPAL_API}/v2/checkout/orders`, order, {
+        auth: {
+            username: PAYPAL_API_CLIENT,
+            password: PAYPAL_API_SECRET
+        }
+    })
 }
 
 export const captureOrder = (req, res) => {
